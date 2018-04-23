37509e797117067de19a7f0a4735988e04c78f17
wufeng1205 <wufeng1205@163.com>
2018-04-23 15:52:33 +0800

    Just a GitHub Test

---
+++ P001.dpr
@@ -0,0 +1,14 @@
+program P001;
+
+uses
+  Vcl.Forms,
+  uMain in 'uMain.pas' {Form17};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm17, Form17);
+  Application.Run;
+end.
---
+++ uMain.pas
@@ -0,0 +1,24 @@
+unit uMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm17 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form17: TForm17;
+
+implementation
+
+{$R *.dfm}
+
+end.
